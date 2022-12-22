05bb9291fd200228ae0f17b345f7b6d1ce58d5b9
Soumya Sah <85212732+smysh@users.noreply.github.com>
2022-12-21 22:21:22 -0800

    added read all books function that queries the database for all records, creates a book_response as a list and jsonifies it.

--- app/routes.py
+++ app/routes.py
@@ -12,12 +12,29 @@ def create_book():
 
     db.session.add(new_book)
     db.session.commit()
-
+    # make_response converts the return value from a view function to an instance of response_class.
+    # str : A response object is created with the string encoded to UTF-8 as the body.
+    # bytes : A response object is created with the bytes as the body.
+    # dict : A dictionary that will be jsonify’d before being returned.
+    # list : A list that will be jsonify’d before being returned.
+    # generator or iterator : A generator that returns str or bytes to be streamed as the response.
+    # tuple : Either (body, status, headers), (body, status), or (body, headers), 
+    # where body is any of the other types allowed here, status is a string or an integer, 
+    # and headers is a dictionary or a list of (key, value) tuples. 
+    # If body is a response_class instance, status overwrites the exiting value and headers are extended.
     return make_response(f"Book {new_book.title} successfully created", 201)
 
 @books_bp.route("", methods=["GET"])
 def read_all_books():
     books_response = []
+    # Flask-SQLAlchemy provides a query attribute on your Model class. 
+    # When you access it you will get back a new query object over all records. e.g.query.all()
+    # You can then use methods like filter() to filter the records before you fire the select with all() or first(). 
+    # e.g. missing = User.query.filter_by(username='missing').first()
+    # e.g. User.query.filter(User.email.endswith('@example.com')).all()
+    # Ordering users by something: e.g. User.query.order_by(User.username).all()
+    # Limiting users: e.g. User.query.limit(1).all()
+    # If you want to go by primary key you can also use get() : e.g. User.query.get(1)
     books = Book.query.all()
     for book in books:
         books_response.append(
@@ -27,6 +44,8 @@ def read_all_books():
                 "description": book.description
             }
         )
+    # Serialize the given arguments as JSON, and return a Response object with the application/json mimetype. 
+    # A dict or list returned from a view will be converted to a JSON respon
     return jsonify(books_response)
 
 #def validate_book(book_id):
