f1a4f407c411da0864cdd2ad158e5bdbc83eb33a
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-01-03 10:28:00 -0800

    added test for get all books with no records

---
+++ tests/test_routes.py
@@ -0,0 +1,9 @@
+# get all books and return no records
+def test_get_all_books_with_no_records(client):
+    # Act
+    response = client.get("/books")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
