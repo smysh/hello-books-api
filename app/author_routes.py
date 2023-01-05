e0466a240af9116f477a64794000f2737f114a11
Soumya Sah <85212732+smysh@users.noreply.github.com>
2023-01-04 23:30:11 -0800

    added create_book and read_books routes

--- app/author_routes.py
+++ app/author_routes.py
@@ -1,5 +1,7 @@
 from app import db
+from app.models.book import Book
 from app.models.author import Author
+from app.book_routes import validate_model
 from flask import Blueprint, jsonify, abort, make_response, request
 
 authors_bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
@@ -27,5 +29,37 @@ def read_all_authors():
             }
         )
     return jsonify(authors_response)
+
+@authors_bp.route("/<author_id>/books", methods=["POST"])
+def create_book(author_id):
+
+    author = validate_model(Author, author_id)
+
+    request_body = request.get_json()
+    new_book =  Book(
+        title=request_body["title"],
+        description=request_body["description"],
+        author=author
+    )
+    db.session.add(new_book)
+    db.session.commit()
+
+    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
+
+@authors_bp.route("/<author_id>/books", methods=["GET"])
+def read_books(author_id):
+
+    author = validate_model(Author, author_id)
+
+    books_response = []
+    for book in author.books:
+        books_response.append(
+            {
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+            }
+        )
+    return jsonify(books_response)
 
 
